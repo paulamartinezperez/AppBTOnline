820cdfa0eb4bd4041e0ebf1881e43592f097bcda
Paula <Paula@DESKTOP-JVAL8CG>
2023-06-04 11:45:37 +0200

    sqllite -> mysql

--- AppBTOnline/Data/PlayerDatabase.cs
+++ AppBTOnline/Data/PlayerDatabase.cs
@@ -1,69 +1,56 @@
-﻿using SQLite;
+﻿using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Pomelo.EntityFrameworkCore.MySql.Storage;
+using Microsoft.EntityFrameworkCore;
 using AppBTOnline.Models;
+using System;
 
 namespace AppBTOnline.Data;
 
 public class PlayerDatabase
 {
-    SQLiteAsyncConnection Database;
+    AppDbContext Database;
     public PlayerDatabase()
     {
-    }
-    async Task Init()
-    {
-        if (Database is not null)
-            return;
+        string connectionString = Constants.DatabasePath;
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 27)));
 
-        Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
-        var result = await Database.CreateTableAsync<Player>();
+        Database = new AppDbContext(optionsBuilder.Options);
     }
 
     public async Task<List<Player>> GetItemsAsync()
     {
-        await Init();
-        return await Database.Table<Player>().ToListAsync();
+        return await Database.Players.ToListAsync();
     }
 
     public async Task<bool> CheckPlayerDetailsAsync(string playerName, string playerLastName, string playerSchool)
     {
-        await Init();
-        var players = await Database.Table<Player>().ToListAsync();
-        return players.Any(player => player.Name.Equals(playerName)  && player.Apellidos.Equals(playerLastName) && player.CentroEducativo.Equals(playerSchool));
+        var players = await Database.Players.ToListAsync();
+        return players.Any(player => player.Name.Equals(playerName) && player.Apellidos.Equals(playerLastName) && player.CentroEducativo.Equals(playerSchool));
     }
-
-
-    //public async Task<List<Player>> GetItemsNotDoneAsync()
-    //{
-    //    await Init();
-    //    return await Database.Table<Player>().Where(t => t.Done).ToListAsync();
-
-    //    // SQL queries are also possible
-    //    //return await Database.QueryAsync<TodoItem>("SELECT * FROM [TodoItem] WHERE [Done] = 0");
-    //}
 
     public async Task<Player> GetItemAsync(int id)
     {
-        await Init();
-        return await Database.Table<Player>().Where(i => i.ID == id).FirstOrDefaultAsync();
+        return await Database.Players.FirstOrDefaultAsync(i => i.ID == id);
     }
 
     public async Task<int> SaveItemAsync(Player item)
     {
-        await Init();
-
         if (item.ID != 0)
         {
-            return await Database.UpdateAsync(item);
+            Database.Players.Update(item);
         }
         else
         {
-            return await Database.InsertAsync(item);
+            Database.Players.Add(item);
         }
+
+        return await Database.SaveChangesAsync();
     }
 
     public async Task<int> DeleteItemAsync(Player item)
     {
-        await Init();
-        return await Database.DeleteAsync(item);
+        Database.Players.Remove(item);
+        return await Database.SaveChangesAsync();
     }
 }
